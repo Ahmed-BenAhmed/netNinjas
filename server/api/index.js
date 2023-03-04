a0b2f5df2c931766e64fc3ccd00e19c9aef376c2
Ahmed BenAhmed <ahmed.tanalt666@gmail.com>
2023-03-04 21:55:36 +0100

    fixing some issues

--- server/api/index.js
+++ server/api/index.js
@@ -1,29 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-import session from "express-session"
-import passport from "passport"
-import userRouter from "../routes/usersRouter.js"
-import cashPackageRouter from "../routes/cashPackagesRouter.js"
-import bodyParser from "body-parser"
-import voyagesRouter from "../routes/VoyagesRouter.js";
 import keys from "../config/keys.js"
-import {passportConfig} from "../config/passport.js"
-import {requireAuth} from "../validation/requireAuth.js";
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-app.use(passport.initialize())
-passportConfig(passport);
-app.use("/api/cashpackage", cashPackageRouter)
-app.use("/api/user", userRouter)
-app.use("/api/voyage", voyagesRouter)
-
-
-
 
 main().then(() => {
       console.log("DB connected");
---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+export default {
+    mongoURI: `mongodb+srv://hero:l91bdAERY2YzmVMc@cluster0.nlvj1gu.mongodb.net/testingApp?retryWrites=true&w=majority`,
+    secretOrKey: "secret"
+};
