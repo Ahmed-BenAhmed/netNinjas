632be7079866f4cae84d14621568f885cd3a37d7
Ahmed BenAhmed <ahmed.tanalt666@gmail.com>
2023-03-20 12:53:29 +0000

    setup files

---
+++ client/src/Components/Project/Detail/DetailProjectPureComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const DetailProjectPureComponent = () => {
+    return <div>
+        <h1>Hello Programmer, I hope you finish your task in the minimum time possible</h1>
+    </div>
+}
---
+++ client/src/Components/Task/Detail/DetailTaskPureComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const DetailTaskPureComponent = () => {
+    return <div>
+        <h1>Hello Programmer, I hope you finish your task in the minimum time possible</h1>
+    </div>
+}
