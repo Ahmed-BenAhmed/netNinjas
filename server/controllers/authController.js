15f6425e60133349d82c5e6ef6d2486b5b5ff077
Abid-Aymen <126987989+Abid-Aymen@users.noreply.github.com>
2023-03-25 17:09:35 +0000

    updated authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,31 +1,32 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-
+;
 //handle errors
 const handleErrors= (err)=>{
     console.log(err.message, err.code);
     let errors = { email: '', password: ''};
     //validation errors
-    if (err.message.includes('user validation failed')) {
+    if (err.message.includes('user validation failed')) 
         (Object.values(err.errors)).forEach(({properties}) => {       
             errors[properties.path] = properties.message;
         })
-    }
-    return errors;
-}
-//duplicate error code
-if (err.code === 11000) {
+    
+   //duplicate error code
+   else if (err.code === 11000) 
     errors.email = 'this email is already registered';
-    return errors;
-}
-//incorrect email 
-if ( err.message === 'incorrect email') {
+    
+   //incorrect email 
+   else if ( err.message === 'incorrect email') 
     errors.email = 'that email is not registered';
-}
-//incorrect password 
-if ( err.message === 'incorrect password') {
+   
+   //incorrect password 
+   else if ( err.message === 'incorrect password') 
     errors.password = 'that password is not registered';
+   
+    return errors;
+    
 }
+
 //creating tokens
 const maxAge = 3*24*60*60;
 const createToken = (id) => {
@@ -69,4 +70,8 @@ module.exports.login_post = async(req, res) => {
         const errors = handleErrors(err); 
         res.status(400).json({errors});
     }
+}
+module.exports.logout_get = (req,res) =>{
+    res.cookie('jwt', '', {maxAge: 1});
+    res.redirect('/login')
 }
