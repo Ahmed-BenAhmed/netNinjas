5a36e5d048abd47c9b3a5315045d2d893abdd7da
Abid-Aymen <126987989+Abid-Aymen@users.noreply.github.com>
2023-04-05 11:33:17 +0000

    change models ....

--- client/src/Components/Task/Detail/DetailTaskPureComponent.tsx
+++ client/src/Components/Task/Detail/DetailTaskPureComponent.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Task } from "../../../shared/model/TaskModel";
+
 
 export const DetailTaskPureComponent = () => {
+
     return <div>
         <h1>Hello Programmer, I hope you finish your task in the minimum time possible</h1>
     </div>
