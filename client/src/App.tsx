59e6d1041ae1704d3a2b8c2bd69bde50bb1dccb4
Abid-Aymen <126987989+Abid-Aymen@users.noreply.github.com>
2023-04-05 22:30:54 +0000

    updated react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import './App.css';
 import { DetailTaskPureComponent } from "./Components/Task/Detail/DetailTaskPureComponent";
-
+import moment from "moment"
 
 
 const App = () => {
-
+    const taskDuration = moment("20230410","YYYYMMDD").fromNow()
     return (
         <div>
             hello world
 
-            <DetailTaskPureComponent />
+            <DetailTaskPureComponent task={{
+                title: "GO Home",
+                group: {
+                    groupName: "Net Ninjas"
+                },
+                duration: taskDuration,
+                description: "This is a group created to participate in CEC competion"
+            }}/>
         </div>
 
   );
