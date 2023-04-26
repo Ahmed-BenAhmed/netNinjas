3c6d9cfb8bd6edcb158c2d1dd2fb8428e3b65b88
Abid-Aymen <126987989+Abid-Aymen@users.noreply.github.com>
2023-04-26 23:08:37 +0100

    added SingUpForm

--- client/src/Components/Auth/SingUpForm.tsx
+++ client/src/Components/Auth/SingUpForm.tsx
@@ -1,8 +1,70 @@
 import React from "react";
-
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from "@reach/router";
 
 export const SingUpForm = () => {
-    return <div>
-        <h1>Hello Programmer, I hope you finish your task in the minimum time possible</h1>
-    </div>
+    const[data,setData]= useState({
+        name : "",
+        email:"",
+        password: ""
+    });
+    const[error, setError]= useState('');
+    const navigate= useNavigate();
+    const handleChange = ({currentTarget: input }) => {
+        setData({...data,[input.name]: input.value});
+    };
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try{
+            const url=""
+            const {data: res} = await axios.post(url,data);
+            navigate("/login");
+            console.log(res.message);
+        }catch(error){
+            if(error.response && error.response.status >=400 && error.response.status <= 500 ){
+                setError(error.response.data.message)
+            }
+        }
+        
+    }
+    return (
+        <div className="SignUpForm_container">
+            <form className= "form_container" onSubmit={handleSubmit}>
+              <h1>Create Account</h1>
+              <input
+               type="text"
+               placeholder="Name" 
+               name="name"
+               onChange={handleChange}
+               value={data.name}
+               required
+               className="input"
+               />
+                 <input
+               type="text"
+               placeholder="Email" 
+               name="email"
+               onChange={handleChange}
+               value={data.email}
+               required
+               className="input"
+               /> 
+                <input
+               type="text"
+               placeholder="Password" 
+               name="password"
+               onChange={handleChange}
+               value={data.password}
+               required
+               className="input"
+               />
+               {error && <div className='errorMessage'>{error}</div>}
+               <button type="submit" className="S_button">
+                Sign Up
+               </button>
+           </form>   
+        </div>
+    )
 }
