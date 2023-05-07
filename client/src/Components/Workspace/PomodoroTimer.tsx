06a8d64035389db1d924f6fac7fdd474de72dfd2
Abid-Aymen <126987989+Abid-Aymen@users.noreply.github.com>
2023-05-07 22:04:37 +0100

    Created a pomodoro timer

--- client/src/Components/Workspace/PomodoroTimer.tsx
+++ client/src/Components/Workspace/PomodoroTimer.tsx
@@ -1,5 +1,79 @@
-import * as React from 'react'
+import React, { useState, useEffect , useRef } from 'react';
+import {Button} from "reactstrap";
 
-export const PomodoroTimer = () => {
+export const PomodoroTimer = ()=> {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
+  let interval = useRef();
+
+  useEffect(() => { 
+     let countdown = setInterval(()=>{
+        if(isActive) {  
+              if(hours === 0 && minutes === 0 && seconds===0){
+              clearInterval(countdown);
+              setIsActive(false);
+            }else if(minutes === 0 && seconds===0){
+              setHours(hours-1);
+              setMinutes(59);
+              setSeconds(59);
+            }else if (seconds===0){
+              setMinutes(minutes-1);
+              setSeconds(59);
+            }else {
+              setSeconds(seconds-1);
+            }}
+      },1000)
+    return ()=> {clearInterval(countdown)}
+    } , [hours, minutes, seconds, isActive]);
+
+  const handleHoursChange = (e) => {
+    setHours(parseInt(e.target.value));
+  };
+
+  const handleMinutesChange = (e) => {
+    setMinutes(parseInt(e.target.value));
+  };
+
+  const handleSecondsChange = (e) => {
+    setSeconds(parseInt(e.target.value));
+  };
+
+  const handleStart = () => {
+    setIsActive(!isActive)
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  const handleStop = () => {
+    setIsActive(false)
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="number" placeholder="hours" value={hours} min="0" max="24" onChange={handleHoursChange} />
+      </div>
+      <div>
+        <input type="number" placeholder="minutes" value={minutes} min="0" max="59" onChange={handleMinutesChange} />
+      </div>
+      <div>
+        <input type="number" placeholder="seconds" value={seconds} min="0" max="59" onChange={handleSecondsChange} />
+      </div>
+      <div>
+       {hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </div>
+      <div>
+        <Button onClick={handleStart}>{isActive? 'stop': 'start'}</Button>
+        <Button onClick={handleReset}>Reset</Button>
+      </div>
+    </div>
+  );
 }
--- client/src/Pages/WorkspacePage.tsx
+++ client/src/Pages/WorkspacePage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { PomodoroTimer } from "../Components/Workspace/PomodoroTimer";
 
 
 export const WorkspacePage = () => {
     return <div>
-        <h1>Hello Programmer, I hope you finish your task in the minimum time possible</h1>
-        <h2>Workspace</h2>
+       <PomodoroTimer />
     </div>
 }
